ccb5f77cd5adaf640ddebff710f7d913b68679be
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-11-11 00:39:20 +0200

    Adding State to Bot

---
+++ PlBot/Services/BotStateService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using PlBot.Models;
+
+namespace PlBot.Services
+{
+    public class BotStateService
+    {
+        #region Variables
+        // State Variables
+        public ConversationState ConversationState { get; }
+        public UserState UserState { get; set; }
+
+        // IDs
+        public static string ConversationDateId { get; } = $"{nameof(BotStateService)}.ConversationDate";
+        public static string UserProfileId { get; } = $"{nameof(BotStateService)}.UserProfile";
+
+        // Accessors
+        public IStatePropertyAccessor<ConversationData> ConversationDataAccessor { get; set; }
+        public IStatePropertyAccessor<UserProfile> UserProfileAccessor { get; set; }
+
+        #endregion
+
+        public BotStateService(ConversationState conversationState, UserState userState)
+        {
+            ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+            UserState = userState ?? throw new ArgumentNullException(nameof(userState));
+
+            InitializeAccessors();
+        }
+
+        public void InitializeAccessors()
+        {
+            // Initialize Conversation State Accessors
+            ConversationDataAccessor = ConversationState.CreateProperty<ConversationData>(ConversationDateId);
+
+            // Initialize User State
+            UserProfileAccessor = UserState.CreateProperty<UserProfile>(UserProfileId);
+        }
+    }
+}
--- PlBot/Startup.cs
+++ PlBot/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using PlBot.Bots;
+using PlBot.Services;
 
 namespace PlBot
 {
@@ -32,8 +33,26 @@ namespace PlBot
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
+            // Configure State
+            ConfigureState(services);
+
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, EchoBot>();
+            services.AddTransient<IBot, GreetingBot>();
+        }
+
+        public void ConfigureState(IServiceCollection services)
+        {
+            // Create the storage we'll be using for User and Conversation state. (Memory is grate for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state.
+            services.AddSingleton<UserState>();
+
+            // Create the Conversation state.
+            services.AddSingleton<ConversationState>();
+
+            // Create an instance of the state service
+            services.AddSingleton<BotStateService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
